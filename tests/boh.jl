0762e44c8b9fcf6419568e1dd8dabfc2ce9c8ba5
Nicholas Mueller <nicholas.mueller@monash.edu>
2025-11-27 10:25:40 +0000

    stash

--- tests/boh.jl
+++ tests/boh.jl
@@ -27,6 +27,14 @@ def mynormal(mu: float, sigma: float, size=(1,)):
   return result
 """ => (myuniform,mynormal)
 
+function pyconvvec(x)
+  pyconvert(Vector{Float64},x)
+end
+
+function pyconvmat(x)
+  Matrix(pyconvert(Matrix{Float64},x)')
+end
+
 # models 
 
 function true_state_transition!(x,states,rainfall,evapcoeff)
@@ -66,14 +74,14 @@ end
 
 function observation!(y,states,measure_noise_std)
   true_observation!(y,states)
-  noise = pyconvert(Vector{Float64},mynormal(0,measure_noise_std,size(y)))
+  noise = pyconvvec(mynormal(0,measure_noise_std,size(y)))
   y .+= noise
   y
 end
 
 function observation(states,measure_noise_std)
   y = true_observation(states)
-  y + pyconvert(Vector{Float64},mynormal(0,measure_noise_std,size(y)))
+  y + pyconvvec(mynormal(0,measure_noise_std,size(y)))
 end
 
 # infos 
@@ -89,15 +97,15 @@ measure_noise_std = 0.5
 inflation_noise_std = 1.0
 
 # generate data 
-rainfall = clamp.(pyconvert(Matrix{Float64},myuniform(0,20,(n,nt))) .- 10.0,0.0,10.0)
-evapcoef = repeat(pyconvert(Vector{Float64},myuniform(0.05,0.1,(n,)));outer=(1,nt))
+rainfall = clamp.(pyconvmat(myuniform(0,20,(nt,n))) .- 10.0,0.0,10.0)
+evapcoef = repeat(pyconvvec(myuniform(0.05,0.1,(n,)));outer=(1,nt))
 
 true_data = zeros(n,nt)
 data = zeros(n,nt)
 obs = zeros(no,nt)
 
-cache_true_data = pyconvert(Vector{Float64},myuniform(20,40,(n,)))
-cache_data = pyconvert(Vector{Float64},myuniform(20,40,(n,)))
+cache_true_data = pyconvvec(myuniform(20,40,(n,)))
+cache_data = pyconvvec(myuniform(20,40,(n,)))
 
 @views for (k,tk) in enumerate(times) 
   true_state_transition!(true_data[:,k],cache_true_data,rainfall[:,k],evapcoef[:,k])
@@ -118,25 +126,23 @@ function enkf_update!(state_ensemble,obs_ensemble,obs,R,measure_noise_std,inflat
   @views for i in axes(state_ensemble,2)
     copyto!(cache,state_ensemble[:,i])
     state_transition!(state_ensemble[:,i],cache,rainfall[:,k],evapcoef[:,k])
-    # state_ensemble[:,i] = state_transition(state_ensemble[:,i],rainfall[:,k],evapcoef[:,k])
     observation!(obs_ensemble[:,i],state_ensemble[:,i],measure_noise_std)
-    # obs_ensemble[:,i] = true_observation(state_ensemble[:,i])#,measure_noise_std) 
   end
 
   Pyy = cov(obs_ensemble')
-  Pxy = (state_ensemble - mean(state_ensemble,dims=2)*o) * (obs_ensemble - mean(obs_ensemble,dims=2)*o)' / sqrt(ne - 1)
+  Pxy = (state_ensemble - mean(state_ensemble,dims=2)*o) * (obs_ensemble - mean(obs_ensemble,dims=2)*o)' / (ne - 1)
   K = Pxy * inv(Pyy + R)
 
   innovation = view(obs,:,k) * o - obs_ensemble
   mul!(state_ensemble,K,innovation,1.0,1.0)
 
-  state_ensemble .+= pyconvert(Matrix{Float64},mynormal(0,inflation_noise_std,size(state_ensemble)))
+  state_ensemble .+= pyconvmat(mynormal(0,inflation_noise_std,(ne,n)))
   
   state_ensemble
 end
 
 R = diagm(measure_noise_std^2*ones(no))
-state_ensemble = pyconvert(Matrix{Float64},myuniform(10,50,(n,ne)))
+state_ensemble = pyconvmat(myuniform(10,50,(ne,n)))
 obs_ensemble = zeros(no,ne)
 
 history = zeros(n,ne,nt)
@@ -146,79 +152,15 @@ for (k,tk) in enumerate(times)
   @views history[:,:,k] = copy(state_ensemble)
 end
 
-posterior_mean = mean(state_ensemble,dims=2)
-posterior_std = std(state_ensemble,dims=2)
+posterior_mean = dropdims(mean(history,dims=2),dims=2)
+posterior_std = dropdims(std(history,dims=2),dims=2)
 
 i = 1
 plot(times,true_data[i,:],color=:black,label="True state")
 plot!(times,posterior_mean[i,:],color=:blue,label="EnKF mean")
-plot!(times,posterior_mean[i,:] .+ xstd[i,:],color=:blue,linestyle=:dash,label="±1 std")
-plot!(times,posterior_mean[i,:] .- xstd[i,:],color=:blue,linestyle=:dash,label="")
+plot!(times,posterior_mean[i,:] .+ posterior_std[i,:],color=:blue,linestyle=:dash,label="±1 std")
+plot!(times,posterior_mean[i,:] .- posterior_std[i,:],color=:blue,linestyle=:dash,label="")
 xlabel!("Time step")
 ylabel!("x[$i]")
 title!("State evolution for variable $i")
 
-##############################################################
-
-# rainfall = 5.0*ones(n,nt)
-# evapcoef = 0.05*ones(n,nt)
-
-# true_data = 20*ones(n,nt)
-# data = 20*ones(n,nt)
-# obs = zeros(no,nt)
-
-# true_data_prev = 20*ones(n)
-# data_prev = 20*ones(n)
-
-# for (k,tk) in enumerate(times) 
-#   true_data[:,k] = true_state_transition(true_data_prev,rainfall[:,k],evapcoef[:,k])
-#   data[:,k] = state_transition(data_prev,rainfall[:,k],evapcoef[:,k])
-#   obs[:,k] = true_observation(true_data[:,k])
-
-#   true_data_prev = copy(true_data[:,k])
-#   data_prev = copy(data[:,k])
-# end
-
-# _true_data = zeros(n,nt)
-# _data = zeros(n,nt)
-# _obs = zeros(no,nt)
-
-# cache_true_data = 20*ones(n)
-# cache_data = 20*ones(n)
-
-# @views for (k,tk) in enumerate(times) 
-#   true_state_transition!(_true_data[:,k],cache_true_data,rainfall[:,k],evapcoef[:,k])
-#   state_transition!(_data[:,k],cache_data,rainfall[:,k],evapcoef[:,k])
-#   true_observation!(_obs[:,k],_true_data[:,k])
-
-#   copyto!(cache_true_data,_true_data[:,k])
-#   copyto!(cache_data,_data[:,k])
-# end
-
-# @assert _true_data ≈ true_data
-# @assert _data ≈ data
-# @assert _obs ≈ obs
-
-# function _enkf_update!(state_ensemble,obs_ensemble,obs,R,k)
-#   n,ne = size(state_ensemble)
-#   o = ones(1,ne)
-
-#   @views for i in axes(state_ensemble,2)
-#     state_ensemble[:,i] = state_transition(state_ensemble[:,i],rainfall[:,k],evapcoef[:,k])
-#     obs_ensemble[:,i] = true_observation(state_ensemble[:,i]) 
-#   end
-
-#   Pyy = cov(obs_ensemble')
-#   Pxy = (state_ensemble - mean(state_ensemble,dims=2)*o) * (obs_ensemble - mean(obs_ensemble,dims=2)*o)' / sqrt(ne - 1)
-#   K = Pxy * inv(Pyy + R)
-
-#   innovation = view(obs,:,k) * o - obs_ensemble
-#   mul!(state_ensemble,K,innovation,1.0,1.0)
-
-#   state_ensemble
-# end
-
-# for (k,tk) in enumerate(times) 
-#   _enkf_update!(state_ensemble,obs_ensemble,obs,R,k)
-#   println(norm(state_ensemble))
-# end
